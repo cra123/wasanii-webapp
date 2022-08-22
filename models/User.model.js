071f81f558079ab44f5231df5afba5947e2dde69
cra123 <Onixmohamed@yahoo.fr>
2022-08-22 17:18:00 +0300

    Checked Sign in page

---
+++ models/User.model.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    address: { type: String, required: true },
+    art: [{ type: Schema.Types.ObjectId, ref: "Art" }],
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
